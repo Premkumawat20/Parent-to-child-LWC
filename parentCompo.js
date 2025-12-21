86540bfaf70e917940c0e701955de4af9c8e65f5
Premkumawat20 <70765548+Premkumawat20@users.noreply.github.com>
2025-12-21 23:43:07 +0530

    Add files via upload

---
+++ parentCompo.js
@@ -0,0 +1,28 @@
+import { LightningElement,api } from 'lwc';
+
+export default class ParentCompo extends LightningElement {
+     progress=10
+   
+    CarosalDetails=[
+        { 
+               src:'https://v1.lightningdesignsystem.com/assets/images/carousel/carousel-01.jpg',
+               header:'First Card',
+               description:'First card description'
+        },
+        {
+               src:'https://v1.lightningdesignsystem.com/assets/images/carousel/carousel-02.jpg',
+               header:'Second Card',
+               description:'Second card description'
+        },
+        {  
+               src:'https://v1.lightningdesignsystem.com/assets/images/carousel/carousel-03.jpg',
+               header:'Third Card',
+               description:'Third card description'
+        }
+    ]
+
+    eventHandler(event){
+     this.progress=event.target.value
+    }
+
+}
